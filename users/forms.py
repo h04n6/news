96fe11d41208d53e4d8728ba67698da59830643e
hoanghd <haduchoang@cryptopie-labo.com>
2023-10-28 23:50:19 +0700

    init

---
+++ users/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from users.models import CustomUser
+
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
